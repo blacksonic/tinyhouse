808a2c4844fe3f3b11ed79251075d003893a15f3
blacksonic <soos.gabor86@gmail.com>
2021-03-13 20:59:42 +0100

    add deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import { ApolloServer } from 'apollo-server-express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import compression from 'compression';
 
 dotenv.config({});
 
@@ -18,9 +19,12 @@ export const createApp = (db: Database): Application => {
 
   app.use(bodyParser.json({ limit: '2mb' }));
   app.use(cookieParser(process.env.SECRET));
-  server.applyMiddleware({ app, path: '/api' });
   app.use(express.json());
   app.use(cors());
+  app.use(compression());
+  app.use(express.static(`${__dirname}/client`));
+  app.get('/*', (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
+  server.applyMiddleware({ app, path: '/api' });
   app.get('/', (req, res) => res.send('hello world'));
   app.post('/statusDone', (req, res) =>
     res.send({
